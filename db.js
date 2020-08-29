ff912e6e437a207a2ab5fcb21e575d1b03161dd7
amine <amine.louni.dev@gmail.com>
2020-08-29 16:11:06 +0100

    database connection

---
+++ db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const DB_URI = process.env.DB_URI.replace(
+  '<dbname>',
+  process.env.DB_NAME
+).replace('<password>', process.env.DB_PASS);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Database connected ✅');
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./db');
 
 const app = express();
 
+// Connect database
+connectDB();
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`The server is running on port ${PORT} ✔ `));
