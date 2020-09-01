e0cc6f30d01083f908c6c18f19d9c40131e902d3
amine <amine.louni.dev@gmail.com>
2020-09-01 21:57:05 +0100

    reinit client

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Button } from 'rsuite';
-// import default style
-import 'rsuite/lib/styles/themes/dark/index.less';
+import logo from './logo.svg';
+import './App.css';
 
-/** import default css */
-// import 'rsuite/dist/styles/rsuite-default.css';
-
-/** import dark css */
-// import 'rsuite/dist/styles/rsuite-dark.css';
 function App() {
   return (
     <div className="App">
-      <h2>resuite js</h2>
-      <Button>Hello Worldd</Button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
